91fea9a6a3da12436e7e7036b6022f2bb574b717
Marian Kulisch <s681562@gmail.com>
2018-11-24 19:11:48 +0900

    change user role

---
+++ api/classes/admin/UserRole.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace admin;
+
+class UserRole {
+	public function __construct() {
+		
+	}
+	
+	public function execute($userId, $roleId) {
+		$items = array("USER", "DESIGNER", "SUPERVISOR", "ADMIN");
+		$roles = array();
+		
+		for($index = 1; $index <= count($items); $index++) {
+			$obj = new \stdClass;
+			$obj->roleId = $index;
+			$obj->roleName = $items[$index - 1];
+			$obj->selected = ($roleId == $index);
+			
+			array_push($roles, $obj);
+		}
+		
+		echo json_encode($roles, JSON_UNESCAPED_UNICODE);		
+	}
+}
